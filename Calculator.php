09cb4b13bfda750a448ae597acb4ea67e025f86d
ZidZaky <zzidanirfan@gmail.com>
2025-04-10 14:20:44 +0700

    Pushing

---
+++ Calculator.php
@@ -0,0 +1,27 @@
+<?php
+
+class CalculatorClass
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function divide($a, $b)
+    {
+        if ($b == 0) {
+            throw new Exception("Division by zero is not allowed.");
+        }
+        return $a / $b;
+    }
+}
